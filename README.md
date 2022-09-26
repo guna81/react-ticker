b0ee90d142c1925ba6aabb908b18a9b501fc6a2b
Guna P <gunaps810@gmail.com>
2022-09-26 16:04:48 +0530

    custom component and custom styles implemented

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# A Simple React Ticker Component | News Ticker
+
+# React Ticker
+
+A simple and flexible react ticker (news slider) component.
+
+
+## Installation
+
+Install with npm
+
+```bash
+  npm install @guna81/react-ticker
+```
+    
+## Usage/Examples
+
+```javascript
+import { ReactTicker } from "@guna81/react-ticker";
+
+function App() {
+  return (
+    <ReactTicker
+      data={news}
+      component={renderItem}
+      speed={30}
+      diretion="normal"
+      keyName="_id"
+      tickerClassName="ticker"
+      itemClassName="ticker__item"
+      tickerTextClassName="ticker__text"
+      tickerStyle={{ height: "30px", backgroundColor: "#E03100" }}
+      itemStyle={{ color: "#fff", fontSize: "14px", fontWeight: "bold" }}
+    />
+  )
+}
+```
+
+
+## API Reference
+
+#### Props
+
+| Parameter | Type     | Description                |
+| :-------- | :------- | :------------------------- |
+| `data` | `array` | **Required**. list of *objects* or *strings* |
+| `component` | `react node` | custom ticker item component |
+| `keyName` | `string` | unique key for of react component |
+| `speed` | `number` | sliding speed  |
+| `delay` | `number` | slide starting delay |
+| `diretion` | `string` | slide direction |
+| `tickerClassName` | `string` |ticker container class name |
+| `itemClassName` | `string` | ticker item class name |
+| `tickerTextClassName` | `string` | ticker text class name (only if data is list of strings) |
+| `tickerStyle` | `object` | ticker container style |
+| `itemStyle` | `object` | ticker item styles |
+
+
+- Componet and keyName props are required if data is given as list of objects.
+- If data is given as list of strings component and keyName props are invalid
--- src/ReactTicker.js
+++ src/ReactTicker.js
@@ -1,47 +1,96 @@
 import React from "react";
+import PropTypes from 'prop-types';
+
 import styles from "./ticker.module.css";
 
 function ReactTicker(props) {
     const {
         data,
+        component,
+        keyName,
+        speed,
         delay,
+        diretion,
         tickerClassName,
         itemClassName,
-        tickerTextClassName
+        tickerTextClassName,
+        tickerStyle,
+        itemStyle,
     } = props;
 
+
     return (
-        <div className={[styles.newsTicker, tickerClassName].join(" ")}>
+        <div
+            className={[styles.newsTicker, tickerClassName].join(" ")}
+            style={tickerStyle}
+        >
             <div
                 className={styles.tickerContent}
                 style={{
-                    animationDuration: `${data.length * 16}s`,
+                    animationDuration: `${data.length * (speed)}s`,
                     animationDelay: `${delay}s`,
+                    animationDirection: diretion,
                 }}
             >
-                {data.map((item) => {
-                    return (
-                        <div key={item} className={[styles.tickerItem, itemClassName].join(" ")}>
-                            <div className={[styles.tickerText, tickerTextClassName].join(" ")}>{item}</div>
-                        </div>
-                    );
-                })}
+                {
+                    component ?
+                        data.map((item, index) => {
+                            return (
+                                <div
+                                    key={keyName ? data[keyName] : index}
+                                    className={[styles.tickerItem, itemClassName].join(" ")}
+                                    style={itemStyle}
+                                >
+                                    {component(item, index)}
+                                </div>
+                            )
+                        })
+                        :
+                        data.map((item) => {
+                            return (
+                                <div
+                                    key={item}
+                                    style={itemStyle}
+                                    className={[styles.tickerItem, itemClassName].join(" ")}
+                                >
+                                    <div className={[styles.tickerText, tickerTextClassName].join(" ")}>{item}</div>
+                                </div>
+                            );
+                        })
+                }
             </div>
         </div>
     );
 }
 
+
 ReactTicker.defaultProps = {
     data: [],
+    component: null,
+    keyName: null,
+    speed: 16,
     delay: 0,
+    direction: "normal",
     tickerClassName: "",
     itemClassName: "",
     tickerTextClassName: "",
+    tickerStyle: {},
+    itemStyle: {},
 }
 
-// BaseTicker.propTypes = {
-//     data: PropTypes.array
-// }
+ReactTicker.propTypes = {
+    data: PropTypes.array.isRequired,
+    component: PropTypes.elementType,
+    keyName: PropTypes.string,
+    speed: PropTypes.number,
+    delay: PropTypes.number,
+    direction: PropTypes.oneOf(["normal", "reverse", "alternate", "alternate-reverse"]),
+    tickerClassName: PropTypes.string,
+    itemClassName: PropTypes.string,
+    tickerTextClassName: PropTypes.string,
+    tickerStyle: PropTypes.object,
+    itemStyle: PropTypes.object,
+}
 
 
 export default ReactTicker;
